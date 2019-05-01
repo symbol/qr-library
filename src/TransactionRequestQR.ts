320121406566177be3f3fd2b99426775d55a366a
Greg S <greg@evias.be>
2019-05-01 18:04:35 +0200

    nemtech/NIP#21 : WIP : refactored classes and package, removed ASCII tests

--- src/TransactionRequestQR.ts
+++ src/TransactionRequestQR.ts
@@ -1,83 +1,56 @@
-/*
- * Copyright (c) 2019 Anthony Law for NEM Foundation
+/**
+ * Copyright 2019 NEM
  *
- * Licensed under the Apache License, Version 2.0 (the "License ");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ *limitations under the License.
  */
-
-import {QRScheme} from "./QrScheme";
-import {QRCodeType} from './QrCodeType'
-import qrcode from 'qrcode-generator';
+import * as QRCode from 'qrcode-generator';
 import {
     TransactionMapping,
     Transaction
 } from "nem2-sdk";
 
+// internal dependencies
+import {
+    QRCodeBase,
+    QRCodeInterface,
+    QRCodeType,
+    QRCodeSettings,
+} from '../index';
 
-export class TransactionRequestQR implements QRScheme{
-
-    protected readonly errorCorrectLevel: any = 'L'; // Error correction level ('L', 'M', 'Q', 'H')
-    protected readonly typeNumber: any = 0; // Type number (1 ~ 40), or 0 for auto detection
-    protected readonly qrCellSizePixel: number = 5;
-    protected readonly qrMarginPixel: number  = 5;
-
+export class TransactionRequestQR extends QRCodeBase implements QRCodeInterface {
     /**
-     *
-     * @param data serialized data.
-     * @param endpoint url for network node.
+     * Construct a Transaction Request QR Code out of the
+     * nem2-sdk Transaction instance.
+     * 
+     * @param   transaction     {Transaction}
      */
-    constructor(public readonly data: string,
-        public readonly endpoint: string) {}
-
-    /**
-     * Create Transaction by serialized data.
-     *
-     * @return  {Transaction}
-     */
-    public toTransaction(): Transaction {
-        return TransactionMapping.createFromPayload(this.data);
+    constructor(/**
+                 * The transaction for the request.
+                 * @var {Transaction}
+                 */
+                public readonly transaction: Transaction) {
+        super(QRCodeType.TransactionRequest);
     }
 
     /**
-     * Build QR schema in string format.
+     * The `toJSON()` method should return the JSON
+     * representation of the QR Code content.
      *
-     * @return  {string}
+     * @return {string}
      */
-    public build(): string {
-        let schema = `{"schema":${QRCodeType.TransactionRequest},"network_id": ${this.toTransaction().networkType},"endpoint":"${this.endpoint.toString()}","data": ${JSON.stringify(this.toTransaction())}}`
-        return schema.trim();
-    }
-
-    /**
-     * Generate QR code in ASCII format.
-     *
-     * @return  {string} Return ASCII format, use in Terminal.
-     */
-    public toQR(): string {
-        let qr = qrcode(this.typeNumber, this.errorCorrectLevel);
-        qr.addData(this.build().replace(/ /g,''));
-        qr.make();
-        return qr.createASCII();
-    }
-
-    /**
-     * Generate QRcode image Base64.
-     *
-     * @return  {string} Retrun image data in Base64.
-     */
-    public toBase64() : string {
-        let qr = qrcode(this.typeNumber, this.errorCorrectLevel);
-        qr.addData(this.build().replace(/ /g,''));
-        qr.make();
-        return qr.createDataURL(this.qrCellSizePixel,this.qrMarginPixel);
+    public toJSON(): string {
+        return JSON.stringify(
+            this.transaction.toJSON()
+        );
     }
 }
